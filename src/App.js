e9e7f2f16b57ded250128aac8b9e67e9df2dfcf4
Omer Mizrachi <omermizra@gmail.com>
2021-04-24 18:09:04 +0300

    reverting again

--- src/App.js
+++ src/App.js
@@ -9,26 +9,10 @@ import Register from './containers/Register';
 import NotFound404 from './containers/NotFound404';
 import './App.css';
 import Amplify, { Auth } from 'aws-amplify';
-<<<<<<< HEAD
 // import awsmobile from './aws-exports';
 
 // Fix configuration only locally - not during deployment.
 // Amplify.configure(awsmobile);
-=======
-import awsmobile from './aws-exports';
-Amplify.configure(awsmobile);
->>>>>>> parent of 6f68886... removed aws-config import
-
-// var aws_exports
-// try {
-// 	aws_exports = require('./aws-exports')
-// } catch(_) {
-// 	aws_exports = null
-// }
-
-// if(aws_exports) {
-// 	Amplify.configure(aws_exports.awsmobile)
-// }
 
 function App() {
 	const [user, setUser] = useState(null)
